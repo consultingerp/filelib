0c43990ff8b6dc9c2df06c77e8c31064a656f9aa
joytao.zhu <joytao.zhu@icloud.com>
2019-04-17 11:20:47 +0800

    参考资-9 - 邮件配置

---
+++ e2yun_addons/odoo11/project_mail_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'project mail fix',
+    'category': 'Project',
+    'description': """
+This module fixbug on field type_id.
+=================================================
+    """,
+    'website': 'https://www.odoo.com/page/project-management',
+    'depends': [
+        'project',
+    ],
+    'data': [
+    ],
+    'auto_install': False,
+}
