bd1c418edee50dde05ae05b279fe4915049fe787
joytao.zhu <joytao.zhu@icloud.com>
2019-06-07 10:53:32 +0800

    fix bug on app_odoo_customize

--- e2yun_addons/odoo12/app_odoo_customize/models/publisher_warranty_contract.py
+++ e2yun_addons/odoo12/app_odoo_customize/models/publisher_warranty_contract.py
@@ -19,7 +19,7 @@ class PublisherWarrantyContract(models.AbstractModel):
         is_enterprise = version_info[5] == 'e'
         _logger.debug('is_enterprise=%s', is_enterprise)
         # Running Odoo EE without calling super is illegal. So, make it impossible to disable in enterprise. See README.rst for details
-        if is_enterprise and self.env['ir.config_parameter'].get_debranding_parameters().get('app_send_publisher_warranty_url') == '1':
+        if is_enterprise and self.env['ir.config_parameter'].get_param('app_send_publisher_warranty_url',default='0') == '1':        
             return super(PublisherWarrantyContract, self).update_notification(cron_mode)
         else:
             return True
