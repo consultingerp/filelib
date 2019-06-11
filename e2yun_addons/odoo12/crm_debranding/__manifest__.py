cc1b2ddfac42a4d41f70858eea4d182e49af6d90
liqiang <qiang.li3@pactera.com>
2019-06-11 11:29:41 +0800

    新增模块--CRM相关odoo标签替换

---
+++ e2yun_addons/odoo12/crm_debranding/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "CRM Backend debranding",
+    'version': '12.0.1.0.27',
+    'author': 'liqiang',
+    'license': 'LGPL-3',
+    'category': 'Debranding',
+    'website': 'https://e2yun.com',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        #'views.xml',
+    ],
+    'auto_install': False,
+    'installable': True
+}
