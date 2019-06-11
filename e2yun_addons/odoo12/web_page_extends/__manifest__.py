d95648297f9071c1cc7caddccdbe7891727e8624
liqiang <qiang.li3@pactera.com>
2019-06-11 11:26:42 +0800

    新增模块--tree thead和tfoot固定

---
+++ e2yun_addons/odoo12/web_page_extends/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Web Page extends",
+    'version': '12.0.1.0.27',
+    'author': 'liqiang',
+    'license': 'LGPL-3',
+    'category': 'Debranding',
+    'website': 'https://e2yun.com',
+    'depends': [
+        'web',
+        'base',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'auto_install': False,
+    'installable': True
+}
