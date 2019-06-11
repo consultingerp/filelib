2e74aa2d5c8ef57ce38c602f4d562bb42ba95e93
liqiang <qiang.li3@pactera.com>
2019-06-11 18:12:01 +0800

    销售订单增强-价格表默认用公司价格表

---
+++ e2yun_addons/odoo12/e2yun_sale_order_extends/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun Sale Order Extends',
+    'version': '12.0.0.1',
+    'sequence': '10',
+    'category': 'sale',
+    'depends': ['sale'],
+    'author': 'liqiang',
+    'website': 'http://e2yun.cn',
+    'summary': 'Sale Order Extends',
+    'description': """Sale Order Extends""",
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
