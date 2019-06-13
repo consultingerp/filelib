ef3b501cbe34c87927978120acacf25ef7504a77
joytao.zhu <joytao.zhu@icloud.com>
2019-06-13 16:29:07 +0800

    参考资-101bing map 定位

---
+++ e2yun_addons/odoo12/e2yun_base_geolocalize_bing/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'E2yun Partners Geolocation Bing',
+    'version': '2.0',
+    'category': 'Sales',
+    'description': """
+E2yun Partners Geolocation use bing
+===================================
+    """,
+    'depends': ['base','base_geolocalize'],
+    'data': [
+    ],
+    'installable': True,
+}
