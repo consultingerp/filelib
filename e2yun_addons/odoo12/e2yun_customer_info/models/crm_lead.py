661793b8d3d29fdc1bc20523c70c12b47581d625
chuqiu.xu <chuqiu.xu@pactera.com>
2019-06-14 14:52:25 +0800

    销售的团队的上级团队

--- e2yun_addons/odoo12/e2yun_customer_info/models/crm_lead.py
+++ e2yun_addons/odoo12/e2yun_customer_info/models/crm_lead.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class crm_lead(models.Model):
     _inherit = 'crm.lead'
 
-    parent_team_id = fields.Many2one(comodel_name='crm.team', compute='_compute_parent_team_id', store=True)
+    parent_team_id = fields.Many2one(comodel_name='crm.team', string='Parent Team id',compute='_compute_parent_team_id', store=True)
 
     @api.one
     @api.depends('team_id')
