922bd34091ec082ca2db269d884392271321194f
joytao.zhu <joytao.zhu@icloud.com>
2019-06-16 20:44:41 +0800

    参考资-107百度必应地图定位 添加翻译和说明

--- e2yun_addons/odoo12/e2yun_base_geolocalize/__manifest__.py
+++ e2yun_addons/odoo12/e2yun_base_geolocalize/__manifest__.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'E2yun Partners Geolocation',
+    'name': 'E2yun Base Geolocation',
     'version': '2.0',
     'category': 'Sales',
     'description': """
-E2yun Partners Geolocation 
-==========================
-partner.country_id.code == 'CN' use Baidu Geolocation
+======================
+E2yun Base Geolocation
+======================
+This module extends Partners Geolocation
+
+
+if partner.country_id.code == 'CN' use Baidu Geolocation
 others use Bing Geolocation
+
+
+replace website google map with baidu map
+
+
+add Geolocation map for partner view 
     """,
     'depends': ['base','base_geolocalize','e2yun_geoengine_maps'],
     'data': ['views/res_partner_views.xml',
