c6228cd6b096abe480daf32ecef2bf8c82987562
liqiang <qiang.li3@pactera.com>
2019-06-16 11:28:47 +0800

    CRM线索模块增强，添加字段，视图，权限控制

---
+++ e2yun_addons/odoo12/e2yun_crm_lead_extends/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun CRM Lead Extends',
+    'version': '12.0.0.1',
+    'sequence': '10',
+    'category': 'CRM',
+    'depends': ['crm','product'],
+    'author': 'liqiang',
+    'website': 'http://e2yun.cn',
+    'summary': 'CRM Lead Extends',
+    'description': """CRM Lead Extends""",
+    'data': [
+        'views/crm_lead_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
