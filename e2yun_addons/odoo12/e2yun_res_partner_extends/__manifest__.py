c93ffa50c9258f7f85c59bb1e3d9e4320b71c082
liqiang <qiang.li3@pactera.com>
2019-06-16 11:30:02 +0800

    CRM客户增强，增加字段，权限控制

---
+++ e2yun_addons/odoo12/e2yun_res_partner_extends/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun Res Partner Extends',
+    'version': '12.0.0.1',
+    'sequence': '10',
+    'category': 'base',
+    'depends': ['base'],
+    'author': 'liqiang',
+    'website': 'http://e2yun.cn',
+    'summary': 'Res Partner Extends',
+    'description': """Res Partner Extends""",
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
