0f2e1cf813d214cf5d0539b7f7444fe616b2553b
zhoufeng <feng.zhou1@pactera.com>
2019-06-19 15:27:46 +0800

    控制mail.activity的权限控制，不用系统的create_uid

---
+++ e2yun_addons/odoo12/e2yun_mail_activity_extends/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'E2yun mail activity Extends',
+    'summary': 'Extend mail activity rewrite create method',
+    'version': '12.0.1.0.0',
+    'author': 'Feng Zhou',
+    'website': 'https://www.e2yun.com',
+    'category': 'Tools',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/access_restricted_security.xml'
+    ],
+    'license': 'AGPL-3',
+}
