70af98d22e8fb834d0621160fce43682458ef357
Kangyu.Wang <kangyu.wang@pactera.com>
2019-06-21 18:16:18 +0800

    客户主数据扩充

---
+++ e2yun_addons/odoo12/e2yun_csutomer_extends/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "E2yun Csutomer Extends",
+
+    'summary': """
+        客户主数据扩充""",
+
+    'description': """
+        客户主数据扩充
+    """,
+
+    'author': "Kangyu.Wang",
+    'website': "http://www.e2yun.cn",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
