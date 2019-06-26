65dad915a9d40d0116c109559535b4a01cf24e55
Kangyu.Wang <kangyu.wang@pactera.com>
2019-06-26 12:34:26 +0800

    APP编码字段改为系统自动给号，可配置 翻译调整

--- e2yun_addons/odoo12/e2yun_csutomer_extends/models/models.py
+++ e2yun_addons/odoo12/e2yun_csutomer_extends/models/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class E2yunCsutomerExtends(models.Model):
     _inherit = 'res.partner'
 
-    app_code = fields.Char(string='')
+    app_code = fields.Char(string='', copy=False, readonly=True, default=lambda self: _('New'))
     shop_code = fields.Char(string='')
     shop_name = fields.Char(string='')
     referrer = fields.Many2one('res.user', string='')
@@ -20,6 +20,18 @@ class E2yunCsutomerExtends(models.Model):
         ('barcode', 'Barcode'),
         ('manual', 'Manual'),
     ], string='', default='manual')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('app_code', _('New')) == _('New'):
+            if 'company_id' in vals:
+                vals['app_code'] = self.env['ir.sequence'].with_context(force_company=vals['company_id'])\
+                                   .next_by_code('app.code') or _('New')
+            else:
+                vals['app_code'] = self.env['ir.sequence'].next_by_code('app.code') or _('New')
+
+        result = super(E2yunCsutomerExtends, self).create(vals)
+        return result
 
 #     name = fields.Char()
 #     value = fields.Integer()
