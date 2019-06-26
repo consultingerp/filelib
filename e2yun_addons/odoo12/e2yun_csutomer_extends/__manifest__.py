703ff15c85b4476ed7a871e82e44be70f9d81966
Kangyu.Wang <kangyu.wang@pactera.com>
2019-06-26 12:34:26 +0800

    APP编码字段改为系统自动给号，可配置 翻译调整

--- e2yun_addons/odoo12/e2yun_csutomer_extends/__manifest__.py
+++ e2yun_addons/odoo12/e2yun_csutomer_extends/__manifest__.py
@@ -25,6 +25,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'data/ir_sequence_data.xml'
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
