c212786151d0de49ff1dbe07763abc883175a276
chuqiu.xu <chuqiu.xu@pactera.com>
2019-06-27 17:29:39 +0800

    机会合同报表

---
+++ e2yun_addons/odoo12/e2yun_business_opportunity_report/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun CRM Business opportunity report',
+    'version': '12.0.0.1',
+    'depends': ['base','crm','crm_lead_product'],
+    'author': 'chuqiu.xu',
+    'website': 'http://e2yun.cn',
+    'summary': 'CRM Business opportunity report',
+    'description': """CRM Business opportunity report""",
+    'data': [
+        'views/business_opportunity_report.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
