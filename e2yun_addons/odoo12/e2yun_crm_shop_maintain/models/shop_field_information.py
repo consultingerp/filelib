08eb63fe94c0e76004cc14c543e355dcdc18bf85
Li Haonan <haonan.li3@pactera.com>
2019-06-28 11:32:54 +0800

    地址样式改为分栏； 修改门店编码位置； 修正原生字段只在开发者模式下才显示的问题；

--- e2yun_addons/odoo12/e2yun_crm_shop_maintain/models/shop_field_information.py
+++ e2yun_addons/odoo12/e2yun_crm_shop_maintain/models/shop_field_information.py
@@ -4,10 +4,9 @@ from odoo import api, fields, models, _
 
 
 class CrmTeamADDinformation(models.Model):
-    _inherit = 'crm.team'
+    _inherit = ['crm.team']
 
-    team_type = fields.Char()
-
+    shop_type = fields.Char('门店类别')
     shop_code = fields.Char('门店编码')
     sales_organization = fields.Char('销售组织')
     sales_organization_name = fields.Char('销售组织名称')
@@ -17,6 +16,13 @@ class CrmTeamADDinformation(models.Model):
     address = fields.Char('详细地址')
     tel = fields.Char('联系电话')
 
+    street = fields.Char('地址')
+    street2 = fields.Char('地址2')
+    zip = fields.Char('邮编', change_default=True)
+    city = fields.Char('市')
+    state_id = fields.Many2one("res.country.state", string='省', ondelete='restrict',
+                               domain="[('country_id', '=?', country_id)]")
+    country_id = fields.Many2one('res.country', string='国家', ondelete='restrict')
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
