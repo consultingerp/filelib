dcc481168ffcd403a98fce148c0884203e444f0a
Kangyu.Wang <kangyu.wang@pactera.com>
2019-07-04 16:12:08 +0800

    将客户视图独立出来，不采用继承写法

--- e2yun_addons/odoo12/e2yun_customer_extends/__manifest__.py
+++ e2yun_addons/odoo12/e2yun_customer_extends/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'crm', 'sales_team'],
+    'depends': ['base', 'crm', 'sales_team', 'sale', 'base_setup'],
 
     # always loaded
     'data': [
