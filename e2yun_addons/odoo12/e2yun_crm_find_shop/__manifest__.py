e6e804232a049528a8510164a88dd86558270d34
Li Haonan <haonan.li3@pactera.com>
2019-07-05 17:27:58 +0800

    新增看板页面，显示门店信息： crm-销售-门店查询

---
+++ e2yun_addons/odoo12/e2yun_crm_find_shop/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "E2yun Shop Find",
+    "summary": "CRM客户门店查询",
+    "version": "12.0.1.0.0",
+    "category": "CRM",
+    "website": "http://e2yun.com",
+    "author": "Li Haonan, "
+              "e2yun.com ",
+    "license": "LGPL-3",
+    "data": [
+        "views/crm_find_shop.xml",
+    ],
+    "depends": [
+        "crm", "base", "e2yun_crm_shop_maintain", "sales_team"
+    ],
+    "installable": True
+}
