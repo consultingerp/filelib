1a8912573a9b5a76cf4319780770444a46e6336e
joytao.zhu <joytao.zhu@icloud.com>
2019-07-06 22:13:39 +0800

    参考资-59 调整逻辑添加备注

---
+++ e2yun_addons/odoo11/e2yun_cards_ocr/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "E2yun Cards OCR",
+    "summary": "using baidu aip OCR to identify business Cards",
+    "version": "12.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "http://e2yun.com",
+    "author": "Joytao, "
+              "e2yun.com ",
+    "license": "LGPL-3",
+    "data": [
+        "views/contact_import_wizard_view.xml"
+    ],
+    "depends": [
+        "web","contacts"
+    ],
+    "qweb": [
+        "static/src/xml/e2yun_cards_ocr.xml",
+    ],
+    "installable": True,
+}
