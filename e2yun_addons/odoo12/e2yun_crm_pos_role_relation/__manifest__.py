964dd1472d854db127799fcf3c483a5ddbcad730
liqiang <qiang.li3@pactera.com>
2019-07-08 10:13:59 +0800

    增加POS用户和CRM用户权限对应关系模块

---
+++ e2yun_addons/odoo12/e2yun_crm_pos_role_relation/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun CRM Pos Role Relation',
+    'version': '12.0.0.1',
+    'sequence': '10',
+    'category': 'base',
+    'depends': ['base','crm'],
+    'author': 'liqiang',
+    'website': 'http://e2yun.cn',
+    'summary': 'CRM Pos Role Relation',
+    'description': """CRM 和 POS系统角色对应关系维护""",
+    'data': [
+        'views/view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
