7677eb0cd49971fdae7bdc4cadee5eb255d64965
zhoufeng <feng.zhou1@pactera.com>
2019-07-19 22:26:18 +0800

    控制自动弹出的mail.channel的窗口，只有在PC的时候弹出，手机端的页面不弹出界面。

---
+++ e2yun_addons/odoo12/e2yun_mail_channel_extends/README.rst
@@ -0,0 +1,43 @@
+.. image:: https://img.shields.io/badge/licence-LGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   :alt: License: LGPL-3
+
+==============================
+E2yun Mail Channel Extends
+==============================
+E2yun Mail Channel Extends
+
+Requirements
+============
+
+
+
+Configuration
+=============
+
+
+
+Bug Tracker
+===========
+
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Feng Zhou <feng.zhou1@pactera.com>
+
+Maintainer
+----------
+
+.. image:: http://www.e2yun.com:8080/logo.png
+   :alt: Odoo partner
+   :target: http://www.e2yun.com
+
+This module is maintained by the E2yun.
+
+
+To contribute to this module, please visit http://e2yun.com.
---
+++ e2yun_addons/odoo12/e2yun_mail_channel_extends/models/mail_channel.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Niyas Raphy(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import models, api
+
+
+class MailChannelExtends(models.Model):
+    _inherit = 'mail.channel'
+
+    @api.multi
+    def channel_info(self, extra_info=False):
+        channel_infos = super(MailChannelExtends, self).channel_info(extra_info=extra_info)
+        if 'isMobile' in self._context and self._context['isMobile']:
+            for channel in channel_infos:
+                channel['is_minimized'] = False
+        return channel_infos
