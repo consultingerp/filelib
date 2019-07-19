c40205a0adee24238741e4f9f6a4d5795186864c
zhoufeng <feng.zhou1@pactera.com>
2019-07-19 22:48:11 +0800

    测试服务器生效与否。

--- e2yun_addons/odoo12/e2yun_mail_channel_extends/models/mail_channel.py
+++ e2yun_addons/odoo12/e2yun_mail_channel_extends/models/mail_channel.py
@@ -29,6 +29,7 @@ class MailChannelExtends(models.Model):
     @api.multi
     def channel_info(self, extra_info=False):
         channel_infos = super(MailChannelExtends, self).channel_info(extra_info=extra_info)
+        print('+++++++++++++++++++++++++++++++++++++++++++++++++++' + self._context['isMobile'])
         if 'isMobile' in self._context and self._context['isMobile']:
             for channel in channel_infos:
                 channel['is_minimized'] = False
