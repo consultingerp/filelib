9ae28d1d4fa11b4904d79c017e742f1ca1746f0f
liqiang <qiang.li3@pactera.com>
2019-07-23 16:51:59 +0800

    增加客户地址解析功能

--- e2yun_addons/odoo12/e2yun_partner_address/models/models.py
+++ e2yun_addons/odoo12/e2yun_partner_address/models/models.py
@@ -10,3 +10,52 @@ class E2yunCsutomerExtends(models.Model):
 
     city_id = fields.Many2one('res.state.city',string='City',domain="[('state_id','=?',state_id)]")
     area_id = fields.Many2one('res.city.area',string='Area',domain="[('city_id','=?',city_id)]")
+    address_desc = fields.Text(string='地址解析')
+
+    def get_add(self,desc,s_idx,e_idx,t_name,postfix):
+        d = desc[s_idx:e_idx]
+        if d and e_idx <= len(desc):
+            s = self.env[t_name].search([('name', '=', d)])
+            if s:
+                return s.id,e_idx
+            else:
+                s = self.env[t_name].search([('name', '=', d+postfix)])
+                if s:
+                    return s.id, e_idx
+                else:
+                    return self.get_add(desc,s_idx,e_idx+1,t_name,postfix)
+        else:
+            return 0,0
+
+
+    @api.onchange('address_desc')
+    def analysis_address(self):
+        if self.address_desc:
+
+            adds = self.address_desc.split(' ')
+            if adds[0]:
+                desc = adds[0]
+                desc = desc.replace('省','').replace('市','').replace('区','').replace('县','')
+                state_id,state_idx = self.get_add(desc,0,2,'res.country.state','省')
+                city_id, city_idx = 0,0
+                area_id, area_idx = 0,0
+                street = ''
+                if state_id > 0:
+                    self.state_id = state_id
+                    city_id, city_idx = self.get_add(desc,state_idx,1+state_idx,'res.state.city','市')
+                if city_id > 0:
+                    area_id, area_idx = self.get_add(desc,city_idx,1+city_idx,'res.city.area','区')
+                    self.city_id = city_id
+                    if area_id == 0:
+                        area_id, area_idx = self.get_add(desc, city_idx, 1 + city_idx, 'res.city.area', '县')
+                if area_id:
+                    street = desc[area_idx:len(desc)]
+                    self.area_id = area_id
+                    self.street = street
+            if len(adds) >= 2 and adds[1]:
+                self.name = adds[1]
+            if len(adds) >= 3 and adds[2]:
+                if len(adds[2]) == 11:
+                    self.mobile = adds[2]
+                else:
+                    self.phone = adds[2]
