84a28e4d905cef02c86bc208f5bc826482117ca1
hepeng <hepeng1@163.com>
2019-07-25 11:53:01 +0800

    更新测试发票

--- e2yun_addons/odoo12/wx_tools/models/sale_make_invoice_advance.py
+++ e2yun_addons/odoo12/wx_tools/models/sale_make_invoice_advance.py
@@ -78,6 +78,6 @@ class SaleAdvancePaymentInv(models.TransientModel):
                     template_id = configer_para[0].paraconfig_value
                 url = client.wxenv(
                     self.env).server_url + '/web/login?usercode=saleorderwx&codetype=wx&redirect=' + redirectur
-                client.send_template_message(self, order.partner_id.wx_user_id.openid, template_id, data, url,
+                client.send_template_message(self, order.user_id.wx_user_id.openid, template_id, data, url,
                                              'saleorder')
         return res
