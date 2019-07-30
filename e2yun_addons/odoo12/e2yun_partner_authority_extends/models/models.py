bb4aed66dc6ad1534f5739200f1d7ca2f1583402
Kangyu.Wang <kangyu.wang@pactera.com>
2019-07-30 20:34:32 +0800

    客户主数据权限  加入"担任店长的门店"，"担任片区长的门店"两个字段

--- e2yun_addons/odoo12/e2yun_partner_authority_extends/models/models.py
+++ e2yun_addons/odoo12/e2yun_partner_authority_extends/models/models.py
@@ -34,6 +34,16 @@ class E2yunUserExtends(models.Model):
 
     update_teams_flag = fields.Char('更新团队标识')
 
+    user_id_teams = fields.Many2many( 'crm.team', string='担任店长的门店', relation='e2yun_user_id_user_team_rel',
+        compute='_compute_user_id_crm_teams', store=True)
+
+    update_user_id_teams_flag = fields.Char('担任店长的门店更新标识')
+
+    area_manager_teams = fields.Many2many( 'crm.team', string='担任片区长的门店', relation='e2yun_area_manager_user_team_rel',
+        compute='_compute_area_manager_crm_teams', store=True)
+
+    update_area_manager_teams_flag = fields.Char('担任片区长的门店更新标识')
+
     @api.multi
     @api.depends('update_teams_flag')
     def _compute_crm_teams(self):
@@ -45,6 +55,25 @@ class E2yunUserExtends(models.Model):
                 res += team
             user.teams = res
 
+    @api.multi
+    @api.depends('update_user_id_teams_flag')
+    def _compute_user_id_crm_teams(self):
+        for user in self:
+            res = self.env['crm.team']
+            teams = self.env['crm.team'].search([('user_id.id', '=', user.id)])
+            for team in teams:
+                res += team
+            user.user_id_teams = res
+
+    @api.multi
+    @api.depends('update_area_manager_teams_flag')
+    def _compute_area_manager_crm_teams(self):
+        for user in self:
+            res = self.env['crm.team']
+            teams = self.env['crm.team'].search([('area_manager.id', '=', user.id)])
+            for team in teams:
+                res += team
+            user.area_manager_teams = res
 
 class E2yunCrmTeamExtends(models.Model):
     _inherit = 'crm.team'
@@ -60,4 +89,12 @@ class E2yunCrmTeamExtends(models.Model):
             randon_int_str = str(random.randint)
             for team in self:
                 team.associate_member_ids.write({'update_teams_flag': randon_int_str})
+        if 'user_id' in values:
+            randon_int_str = str(random.randint)
+            for team in self:
+                team.user_id.write({'update_user_id_teams_flag': randon_int_str})
+        if 'area_manger' in values:
+            randon_int_str = str(random.randint)
+            for team in self:
+                team.area_manger.write({'update_area_manger_flag': randon_int_str})
         return res
