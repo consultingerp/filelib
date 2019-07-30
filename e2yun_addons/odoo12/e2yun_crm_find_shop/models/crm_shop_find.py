e9621d68a19e6f6b63336d0ff85460cb3cd36686
Li Haonan <haonan.li3@pactera.com>
2019-07-30 10:10:44 +0800

    取消门店数量 write方法添加return

--- e2yun_addons/odoo12/e2yun_crm_find_shop/models/crm_shop_find.py
+++ e2yun_addons/odoo12/e2yun_crm_find_shop/models/crm_shop_find.py
@@ -41,26 +41,26 @@ class CrmTeamADDinformation(models.Model):
     #     self.with_context(new_ctx)
     #     return res
 
-    @api.model
-    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        # if domain:
-        limit = 8
-            # if ['id', '!=', None] in domain:
-            #     domain.remove(['id', '!=', None])
-            #     domain.append(('city', '=', self.env.user.partner_id.city))
-        #     serach_read = super(CrmTeamADDinformation, self).search_read(domain, fields, offset, limit, order)
-        #     user = self.env.user
-        #     user_city = user.partner_id.city
-        #     crm_team_pool = self.env['crm.team'].search([('city', '=', user_city)])
-        #     team_id = []
-        #     search_read_new = []
-        #     for crm_team in crm_team_pool:
-        #         team_id.append(crm_team.id)
-        #     for item in serach_read:
-        #         if item.get('id') in team_id:
-        #             search_read_new.append(item)
-        #         else:
-        #             continue
-        #     return search_read_new
-        # else:
-        return super(CrmTeamADDinformation, self).search_read(domain, fields, offset, limit, order)
+    # @api.model
+    # def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+    #     # if domain:
+    #     limit = 8
+    #         # if ['id', '!=', None] in domain:
+    #         #     domain.remove(['id', '!=', None])
+    #         #     domain.append(('city', '=', self.env.user.partner_id.city))
+    #     #     serach_read = super(CrmTeamADDinformation, self).search_read(domain, fields, offset, limit, order)
+    #     #     user = self.env.user
+    #     #     user_city = user.partner_id.city
+    #     #     crm_team_pool = self.env['crm.team'].search([('city', '=', user_city)])
+    #     #     team_id = []
+    #     #     search_read_new = []
+    #     #     for crm_team in crm_team_pool:
+    #     #         team_id.append(crm_team.id)
+    #     #     for item in serach_read:
+    #     #         if item.get('id') in team_id:
+    #     #             search_read_new.append(item)
+    #     #         else:
+    #     #             continue
+    #     #     return search_read_new
+    #     # else:
+    #     return super(CrmTeamADDinformation, self).search_read(domain, fields, offset, limit, order)
