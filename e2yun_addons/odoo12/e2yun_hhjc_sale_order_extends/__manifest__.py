14eaaf11bd25a0d3baffaa340e76d5f9ae674449
zhoufeng <feng.zhou1@pactera.com>
2019-10-10 15:34:48 +0800

    宏华销售订单增加pos字段信息。

---
+++ e2yun_addons/odoo12/e2yun_hhjc_sale_order_extends/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun HHJC Sale Order Extends',
+    'version': '12.0.0.1',
+    'sequence': '10',
+    'category': 'sale',
+    'depends': ['sale'],
+    'author': 'FengZhou',
+    'website': 'http://e2yun.cn',
+    'summary': 'Sale Order Extends',
+    'description': """宏华俊成销售订单扩展""",
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
