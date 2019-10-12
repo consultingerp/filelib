c2505bd366d1bb0964b0742e123bcf57fc5c0687
Li_Haonan <haonan.li3@pactera.com>
2019-10-12 10:12:46 +0800

    报表视图调整

--- e2yun_addons/odoo12/outbound_report/models/outbound_final.py
+++ e2yun_addons/odoo12/outbound_report/models/outbound_final.py
@@ -8,13 +8,16 @@ class OutboundFinal(models.Model):
 
     ID = fields.Char('ID')
     salesorderid = fields.Char('销售订单')
-    LFADT = fields.Datetime('日期')
+    LFADT = fields.Datetime('日期', required=True)
     werks = fields.Char('工厂')
     vkorgtext = fields.Char('事业部')
     vtweg = fields.Char('分销渠道')
     ywy = fields.Char('导购员')
     kunnr = fields.Char('门店')
-    jine = fields.Char('金额')
-
+    # jine = fields.Char('金额')
     jiesuanjine = fields.Char('结算小计')
     xiaoshoujine = fields.Char('销售小计')
+
+
+    def xxxx(self):
+        pass
