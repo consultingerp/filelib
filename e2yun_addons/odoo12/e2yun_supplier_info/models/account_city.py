7feca2292f13253645c8053b3873e66b56e5ca89
Zhiyong.Cao <zhiyong.cao@pactera.com>
2019-10-12 14:43:27 +0800

    去除自定义的国家，省份，城市，地区，用强哥写的

--- e2yun_addons/odoo12/e2yun_supplier_info/models/account_city.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class City(models.Model):
-    _name = 'res.state.city'
-    _description = 'City'
-    _order = 'name'
-    name = fields.Char('城市名称', required=True)
-    state_id = fields.Many2one('res.country.state', string='State', required=True)
