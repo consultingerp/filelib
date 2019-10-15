9e9034b5f5b893ed27a8e27a087f5a9e4c4ee874
zhoufeng <feng.zhou1@pactera.com>
2019-10-15 11:04:55 +0800

    input设定直接获取焦点就选中。

--- e2yun_addons/odoo12/e2yun_hhjc_js_extends/static/src/js/my.js
+++ e2yun_addons/odoo12/e2yun_hhjc_js_extends/static/src/js/my.js
@@ -3,13 +3,11 @@ odoo.define('e2yun_hhjc_js_extends.initial_page', function (require) {
 
     // console.log("===========================1111111111111111111111111==================================");
 
-
     var FormView = require('web.FormView');
 
     /**
-     * This file is used to add "message_attachment_count" to fieldsInfo so we can fetch its value for the
-     * chatter's attachment button without having it explicitly declared in the form view template.
-     *
+     * 功能提供了如果是input控件，在初始化form的时候，绑定一个focus事件，能够当获取焦点的时候，就进行全部选中，
+     * 不需要在一个字符一个字符的删除
      */
 
     FormView.include({
