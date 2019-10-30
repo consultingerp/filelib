6a1f1f3118e9c86792814227f3c082a7e3b82087
Zhiyong.Cao <zhiyong.cao@pactera.com>
2019-10-30 17:17:50 +0800

    去掉依赖'base_address_city'

---
+++ e2yun_addons/odoo12/e2yun_srm_partner_address/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "E2yun Partner Address",
+    'summary': """
+        客户地址维护""",
+    'description': """
+        客户地址维护
+    """,
+    'author': "liqiang",
+    'website': "http://www.e2yun.cn",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','contacts','base_address_city'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
