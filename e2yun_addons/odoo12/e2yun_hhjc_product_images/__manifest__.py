7a2d34d81c5d3b352004f1d496cf0cf0f70de5d5
liqiang <qiang.li3@pactera.com>
2019-11-04 11:05:40 +0800

    增加产品图片维护模块

---
+++ e2yun_addons/odoo12/e2yun_hhjc_product_images/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "E2yun HHJC Product imagess",
+    "version": "1.0",
+    "depends": ["base","sale","product"],
+    "category": "tools",
+    "description": """ 
+    本模块主要解决，在odoo中产品多图片的上传，和图片的顺序。
+    
+""",
+    "data": ["views/product_views.xml"],
+    "installable": True
+}
