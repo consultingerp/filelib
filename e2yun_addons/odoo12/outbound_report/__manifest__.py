9e60c5e431c52ee45115bde2c356912d80997921
Li_Haonan <haonan.li3@pactera.com>
2019-11-08 17:00:41 +0800

    群组生成权限模块由8.0升级到12.0

--- e2yun_addons/odoo12/outbound_report/__manifest__.py
+++ e2yun_addons/odoo12/outbound_report/__manifest__.py
@@ -12,7 +12,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/outbound_report_dashboard.xml',
-        # 'views/sales_team_goal.xml',
+        'views/sales_team_goal.xml',
     ],
     'qweb': [
         'static/src/xml/outbound_button_template.xml',
