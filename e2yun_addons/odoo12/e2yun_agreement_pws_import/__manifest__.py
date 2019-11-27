41e256862d8cfe86f75db700ac6504ba82b428d0
chuqiu <981274333@qq.com>
2019-11-27 18:42:44 +0800

    pws解析

---
+++ e2yun_addons/odoo12/e2yun_agreement_pws_import/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun Agreement Pws Import ',
+    'version': '12.0.0.1',
+    'depends': ['agreement'],
+    'author': 'chuqiu.xu',
+    'website': 'http://e2yun.cn',
+    'summary': '',
+    'description': """合同管理pws导入""",
+    'data': [
+        'views/view.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+
+}
