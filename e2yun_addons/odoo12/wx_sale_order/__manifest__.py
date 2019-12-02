5ef81d6e9b9bcf991f453ba66dbbb151908554ad
hepeng <hepeng1@163.com>
2019-12-02 23:33:23 +0800

    微信销售发送微信信息集成

---
+++ e2yun_addons/odoo12/wx_sale_order/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': '销售订单发送微信',
+    'category': 'Website',
+    'summary': '销售订单发送微信 ',
+    'description': """销售订单发送微信.""",
+    'application': True,
+    'depends': [
+        'web','sale_management'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+    ],
+    'qweb': [
+
+    ],
+    'license': 'OEEL-1',
+    'installable': True,
+    'auto_install': False,
+    'active': False,
+    'web': True,
+}
