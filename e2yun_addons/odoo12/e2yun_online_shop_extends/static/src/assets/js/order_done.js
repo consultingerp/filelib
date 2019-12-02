a78fd2e8f400fed4ef032455bdd49e1ea5d13b26
liqiang <qiang.li3@pactera.com>
2019-12-02 15:40:46 +0800

    电商-增加购物车确认，确认成功页面，增加优惠券使用逻辑，页面优化调整

---
+++ e2yun_addons/odoo12/e2yun_online_shop_extends/static/src/assets/js/order_done.js
@@ -0,0 +1,32 @@
+
+function load_order(){
+
+		var access_token = $("input[name='csrf_token']").val();
+		$.post('/e2yun_online_shop_extends/get_order_done_info',{
+			'access_token' : access_token,
+			'csrf_token' : access_token
+		},function(datas){
+			var d = JSON.parse(datas);
+			var total_price = d['total_price'];
+			var order_code = d['order_code'];
+
+			$('.amount').text('￥'+total_price);
+			$("span[name='order_code']").text(order_code);
+		});
+	}
+
+	$(document).ready(function() {
+		$.post('/e2yun_online_shop_extends/get_token',{
+		},function(datas) {
+            var d = JSON.parse(datas);
+            $('body').append("<input name='csrf_token' value="+d['csrf_token']+" type='hidden' />");
+
+            load_order();
+        });
+
+		$("a[name='btn_back']").click(function(){
+			window.location.href = '/hhjc_shop_order_list'
+		})
+
+	});
+
