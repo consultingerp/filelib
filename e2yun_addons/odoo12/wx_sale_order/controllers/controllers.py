4e4588a419952c03e2fab2a111c29dc8634344f8
hepeng <hepeng1@163.com>
2019-12-03 14:48:41 +0800

    微信扫码功能：位于：wx_sale_order模块
    测试菜单：销售--》订单--》扫码测试
    
    扫码需要配置验证域名：
    JSSDK文档： https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html
    现目前在测试机上可以测试
    
    微信JSSDK已集成于request.env.user.get_jsapi_ticket(url_)方法，调用会产生JSSDK，JSSDK已用redies缓存

--- e2yun_addons/odoo12/wx_sale_order/controllers/controllers.py
+++ e2yun_addons/odoo12/wx_sale_order/controllers/controllers.py
@@ -10,3 +10,10 @@ from odoo import http
 
 _logger = logging.getLogger(__name__)
 
+
+class WxSaleOrder(http.Controller):
+    @http.route('/website/saleorder', type='http', auth="public", website=True)
+    def saleorder(self, **kwargs):
+        url_ = request.httprequest.url
+        _logger.info("参数打印:%s" % url_)
+        return request.render("wx_sale_order.order_test", {'jsapi_ticket': request.env.user.get_jsapi_ticket(url_)})
