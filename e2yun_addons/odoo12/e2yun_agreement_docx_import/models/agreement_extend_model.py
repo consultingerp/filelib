752d45955bbccee8835ee68565b1865578b27189
chuqiu <981274333@qq.com>
2019-12-06 10:35:00 +0800

    合同模块docx解析与下载

--- e2yun_addons/odoo12/e2yun_agreement_docx_import/models/agreement_extend_model.py
+++ e2yun_addons/odoo12/e2yun_agreement_docx_import/models/agreement_extend_model.py
@@ -7,7 +7,9 @@ from odoo import api, fields, models, tools, _
 class Agreement(models.Model):  #合同
     _inherit = "agreement"
 
-
+    # def create(self, vals_list):
+    #     print(vals_list)
+    #     return super(Agreement,self).create(vals_list)
 
 
 
@@ -17,18 +19,27 @@ class AgreementRecital(models.Model):  #叙述
     doc_font = fields.Char('Doc Font')
     doc_text = fields.Char('Doc Text')
 
+class AgreementSection(models.Model):  #章节
+    _inherit = "agreement.section"
+    doc_style = fields.Char('Doc Style')
+    doc_font = fields.Char('Doc Font')
+    doc_text = fields.Char('Doc Text')
+
+
+
 class AgreementClause(models.Model):  #条款
     _inherit = "agreement.clause"
     doc_style = fields.Char('Doc Style')
     doc_font = fields.Char('Doc Font')
     doc_text = fields.Char('Doc Text')
 
-
-class AgreementSection(models.Model):  #章节
-    _inherit = "agreement.section"
-    doc_style = fields.Char('Doc Style')
-    doc_font = fields.Char('Doc Font')
-    doc_text = fields.Char('Doc Text')
+    # def create(self, vals_list):
+    #     print(vals_list)
+    #     return super(AgreementClause,self).create(vals_list)
+    #
+    # def write(self, vals_list):
+    #     print(vals_list)
+    #     return super(AgreementClause,self).write(vals_list)
 
 
 
