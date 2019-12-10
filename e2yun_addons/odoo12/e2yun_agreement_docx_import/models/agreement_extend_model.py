a746bfa1f2a4d246869f18990a16ffb68ec17392
chuqiu <981274333@qq.com>
2019-12-10 17:05:21 +0800

    word替换只替换选定内容

--- e2yun_addons/odoo12/e2yun_agreement_docx_import/models/agreement_extend_model.py
+++ e2yun_addons/odoo12/e2yun_agreement_docx_import/models/agreement_extend_model.py
@@ -15,23 +15,19 @@ class Agreement(models.Model):  #合同
 
 class AgreementRecital(models.Model):  #叙述
     _inherit = "agreement.recital"
-    doc_style=fields.Char('Doc Style')
-    doc_font = fields.Char('Doc Font')
     doc_text = fields.Char('Doc Text')
 
 class AgreementSection(models.Model):  #章节
     _inherit = "agreement.section"
-    doc_style = fields.Char('Doc Style')
-    doc_font = fields.Char('Doc Font')
     doc_text = fields.Char('Doc Text')
 
 
 
 class AgreementClause(models.Model):  #条款
     _inherit = "agreement.clause"
-    doc_style = fields.Char('Doc Style')
-    doc_font = fields.Char('Doc Font')
-    doc_text = fields.Char('Doc Text')
+    doc_text = fields.Text('Doc Text')
+    master_word_id = fields.Integer('Master Word Id')
+    #the_editor = fields.Boolean('The Editor')
 
     # def create(self, vals_list):
     #     print(vals_list)
@@ -46,8 +42,6 @@ class AgreementClause(models.Model):  #条款
 class AgreementAppendix(models.Model):  #附录
     _inherit = "agreement.appendix"
     doc_style = fields.Char('Doc Style')
-    doc_font = fields.Char('Doc Font')
-    doc_text = fields.Char('Doc Text')
 
 
 
