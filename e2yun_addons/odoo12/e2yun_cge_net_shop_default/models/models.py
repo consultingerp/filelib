774a7e2a65e02763d27bfcb5eec9704a7f3c1ec1
Kangyu.Wang <kangyu.wang@pactera.com>
2019-12-18 18:26:29 +0800

    远大 在线商城 个人简介  默认公司类型

--- e2yun_addons/odoo12/e2yun_cge_net_shop_default/models/models.py
+++ e2yun_addons/odoo12/e2yun_cge_net_shop_default/models/models.py
@@ -5,6 +5,8 @@ from odoo import models, fields, api
 class e2yun_cge_net_shop_default(models.Model):
     _inherit = 'res.partner'
 
+    is_company = fields.Boolean(default=True)
+
     # @api.model
     # def _set_cge_user_id(self):
     #     flag = self.env.context.get('cge_set_default_user_id', False)
