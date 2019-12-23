8b248f4487a71a6094600318b9e18609ede0cf4e
hepeng <hepeng1@163.com>
2019-12-23 15:40:30 +0800

    _logger.info("订单公司代码%s" % sale_order.comapny_id)             _logger.info("订单网站到%s" % website.id)

--- e2yun_addons/odoo12/e2yun_online_shop_extends/controllers/cart_controllers.py
+++ e2yun_addons/odoo12/e2yun_online_shop_extends/controllers/cart_controllers.py
@@ -181,7 +181,7 @@ class cart(user_info.WebUserInfoController):
             if website:
                 sale_order.website_id = website.id
                 sale_order.company_id = company_id
-            _logger.info("订单公司代码%s" % sale_order.comapny_id)
+            _logger.info("订单公司代码%s" % sale_order.company_id)
             _logger.info("订单网站到%s" % website.id)
 
 
