b3dcee76059eeaf881f89c930bbdaf45d405f7e7
zhoufeng <feng.zhou1@pactera.com>
2020-01-03 15:25:08 +0800

    把修改放到e2yun_dynamic_workflow里面。

--- e2yun_addons/odoo12/e2yun_customer_info/models/models.py
+++ e2yun_addons/odoo12/e2yun_customer_info/models/models.py
@@ -35,21 +35,6 @@ class e2yun_customer_info(models.Model):
     _name = 'e2yun.customer.info'
     _inherit = ['mail.thread']
 
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-        res = super(e2yun_customer_info, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
-        if view_type == 'form':
-            doc = etree.XML(res['arch'])
-            for node in doc.xpath("//field[@name='message_ids']"):
-                # the 'Log a note' button is employee only
-                options = safe_eval(node.get('options', '{}'))
-                is_employee = self.env.user.has_group('base.group_user')
-                options['display_log_button'] = is_employee
-                # save options on the node
-                node.set('options', repr(options))
-            res['arch'] = etree.tostring(doc, encoding='unicode')
-        return res
-
     def _default_category(self):
         return self.env['res.partner.category'].browse(self._context.get('category_id'))
 
