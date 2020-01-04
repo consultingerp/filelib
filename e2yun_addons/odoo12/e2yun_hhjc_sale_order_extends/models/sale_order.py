4629a920921ea7b85dc136e5e87efe7451b93317
Feng Zhou <a4435351@126.com>
2020-01-04 21:27:18 +0800

    增加如果价格表不是公司的，修改价格表信息。

--- e2yun_addons/odoo12/e2yun_hhjc_sale_order_extends/models/sale_order.py
+++ e2yun_addons/odoo12/e2yun_hhjc_sale_order_extends/models/sale_order.py
@@ -119,6 +119,7 @@ class SaleOrder(models.Model):
     def create(self, vals):
         res = super(SaleOrder, self).create(vals)
         try:
+            _logger.info('测试一下是不是进入这个里面：%s,%s' %(res.pricelist_id.company_id,res.company_id))
             if res.pricelist_id.company_id != res.company_id:
                 pricelist = self.env['product.pricelist'].search([('compnay_id', '=', res.company_id.id)], limit=1)
                 res.pricelist_id = pricelist
