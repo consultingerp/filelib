bb4eab922235b603b70ab499b7e4b6498bb82645
chuqiu <981274333@qq.com>
2020-01-06 21:31:19 +0800

    crm sap 客户接口扩展

---
+++ e2yun_addons/odoo12/e2yun_crm_sap_customer_interface_extend/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'E2yun Crm Sap Customer Interface Extend ',
+    'version': '12.0.0.1',
+    'depends': ['e2yun_customer_info'],
+    'author': 'chuqiu.xu',
+    'website': 'http://e2yun.cn',
+    'summary': '',
+    'description': """Crm Sap Customer Interface """,
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+
+}
