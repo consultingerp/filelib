7e4e91094f6409975328ae21fcec296aac0140c3
Li Haonan <haonan.li3@pactera.com>
2020-01-15 15:17:23 +0800

    修改翻译

--- e2yun_addons/odoo12/e2yun_demo_crm_extends/models/models.py
+++ e2yun_addons/odoo12/e2yun_demo_crm_extends/models/models.py
@@ -13,3 +13,13 @@ class e2yun_demo_crm_extends(models.Model):
                                                 ["Public_Relations", "公共关系"],
                                                 ["Exhibition", "展会"]], 'Account Source', track_visibility='onchange')
 
+class e2yun_demo_crm_extend_sres_partner(models.Model):
+    _inherit = "res.partner"
+
+    x_studio_account_type = fields.Selection([["Target Client", "目标客户"], ["Active Client", "活动客户"],
+                                              ["Significant Client", "重要客户"]], 'Account type', track_visibility='onchange')
+
+    x_studio_ = fields.Selection(
+        [["客户类型", "T&M 合约，按月/按季度计费"], ["行业1", "按里程碑计费的 FP"],
+         ["银行", "项目完成后支付工资，项目周期小于2个月"],
+         ["制造业", "项目完成后支付工资，项目周期大于2个月"]], 'Way of settlement', track_visibility='onchange')
