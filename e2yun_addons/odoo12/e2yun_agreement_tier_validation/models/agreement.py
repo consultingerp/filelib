aca37af9810cc655bd883b3475f8faf50c445d12
chuqiu <981274333@qq.com>
2020-01-15 20:44:32 +0800

    审批设置

--- e2yun_addons/odoo12/e2yun_agreement_tier_validation/models/agreement.py
+++ e2yun_addons/odoo12/e2yun_agreement_tier_validation/models/agreement.py
@@ -278,7 +278,7 @@ class TierValidation(models.AbstractModel):
             if (rec.review_ids and getattr(rec, self._state_field) in
                     self._state_from and not vals.get(self._state_field) in
                     (self._state_to + [self._cancel_state]) and not
-                    self._check_allow_write_under_validation(vals)) and flag:
+                    self._check_allow_write_under_validation(vals)):
                 raise ValidationError(_("The operation is under validation."))
         if vals.get(self._state_field) in self._state_from:
             self.mapped('review_ids').unlink()
