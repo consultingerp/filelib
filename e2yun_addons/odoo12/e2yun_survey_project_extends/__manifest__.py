a4638eb8082750df31e641d2169e8e9a9bb81679
Kangyu.Wang <kangyu.wang@pactera.com>
2020-02-17 18:23:58 +0800

    供应商评估 问卷增加历史问卷、模板问题逻辑

--- e2yun_addons/odoo12/e2yun_survey_project_extends/__manifest__.py
+++ e2yun_addons/odoo12/e2yun_survey_project_extends/__manifest__.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "e2yun_survey_eextends",
+    'name': "e2yun_survey_project_extends",
 
     'summary': """
-        问卷修改""",
+        供应商评估  涉及项目模块的更改""",
 
     'description': """
-        问卷修改
-    """,
+        与问卷相关的内容""",
 
-    'author': "Zhiyong.Cao",
+    'author': "Kangyu.Wang",
     'website': "http://www.e2yun.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Survey',
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'survey', 'e2yun_suppliers_register', 'e2yun_survey_pagee_extends'],
+    'depends': ['base', 'project', 'e2yun_project_extends'],
 
     # always loaded
     'data': [
@@ -28,7 +27,7 @@
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
 }
