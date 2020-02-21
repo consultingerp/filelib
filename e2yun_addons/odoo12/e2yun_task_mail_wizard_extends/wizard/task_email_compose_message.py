c9c51a5af728af6d4c0acfea7c652f76139cedc8
czy <13026391531@163.com>
2020-02-21 13:57:40 +0800

    任务邮件优化和分派

--- e2yun_addons/odoo12/e2yun_task_mail_wizard_extends/wizard/task_email_compose_message.py
+++ e2yun_addons/odoo12/e2yun_task_mail_wizard_extends/wizard/task_email_compose_message.py
@@ -7,7 +7,7 @@ import uuid
 
 from werkzeug import urls
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _,exceptions
 from odoo.exceptions import UserError
 from odoo.tools import pycompat
 
@@ -48,7 +48,7 @@ class SurveyMailComposeMessage(models.TransientModel):
     survey_ids = fields.Many2many('survey.survey', string='Survey', default=default_survey_ids, required=True)
     public = fields.Selection([('public_link', 'Share the public web link to your audience.'),
                                 ('email_public_link', 'Send by email the public web link to your audience.'),
-                                ('email_private', 'Send private invitation to your audience (only one response per recipient and per invitation).')],
+                               ('send_internal_process_messages', 'Send internal process messages.')],
                                 string='Share options', default='public_link', required=True)
     public_url = fields.Char(compute="_compute_survey_url", string="Public url")
     public_url_html = fields.Char(compute="_compute_survey_url", string="Public HTML web link")
@@ -60,6 +60,14 @@ class SurveyMailComposeMessage(models.TransientModel):
     date_deadline = fields.Date(string="Deadline to which the invitation to respond is valid",
         help="Deadline to which the invitation to respond for this survey is valid. If the field is empty,\
         the invitation is still valid.")
+
+    @api.onchange('public')
+    def onchange_partner_ids(self):
+        if self.public == 'send_internal_process_messages':
+            domain = [('partner_share', '=', 'True')]
+            return {
+                'domain': {'partner_ids': domain}
+            }
 
     @api.depends('survey_ids')
     def _compute_survey_url(self):
@@ -351,5 +359,91 @@ class SurveyMailComposeMessage(models.TransientModel):
                 create_response_and_send_mail(wizard, token, partner['id'], partner['email'])
 
         return {'type': 'ir.actions.act_window_close'}
+
+    @api.multi
+    def action_send_mail(self):
+        self.mail_send()
+        if not self.partner_ids and not self.multi_email:
+            raise exceptions.Warning(_('Please enter an existing contact or email list'))
+        return {'type': 'ir.actions.act_window_close', 'infos': 'mail_sent'}
+
+    @api.multi
+    def mail_send(self, auto_commit=False):
+        """ Process the wizard content and proceed with sending the related
+            email(s), rendering any template patterns on the fly if needed. """
+        notif_layout = self._context.get('custom_layout')
+        # Several custom layouts make use of the model description at rendering, e.g. in the
+        # 'View <document>' button. Some models are used for different business concepts, such as
+        # 'purchase.order' which is used for a RFQ and and PO. To avoid confusion, we must use a
+        # different wording depending on the state of the object.
+        # Therefore, we can set the description in the context from the beginning to avoid falling
+        # back on the regular display_name retrieved in '_notify_prepare_template_context'.
+        model_description = self._context.get('model_description')
+        for wizard in self:
+            # Duplicate attachments linked to the email.template.
+            # Indeed, basic mail.compose.message wizard duplicates attachments in mass
+            # mailing mode. But in 'single post' mode, attachments of an email template
+            # also have to be duplicated to avoid changing their ownership.
+            if wizard.attachment_ids and wizard.composition_mode != 'mass_mail' and wizard.template_id:
+                new_attachment_ids = []
+                for attachment in wizard.attachment_ids:
+                    if attachment in wizard.template_id.attachment_ids:
+                        new_attachment_ids.append(
+                            attachment.copy({'res_model': 'mail.compose.message', 'res_id': wizard.id}).id)
+                    else:
+                        new_attachment_ids.append(attachment.id)
+                wizard.write({'attachment_ids': [(6, 0, new_attachment_ids)]})
+
+            # Mass Mailing
+            mass_mode = wizard.composition_mode in ('mass_mail', 'mass_post')
+
+            Mail = self.env['mail.mail']
+            ActiveModel = self.env[wizard.model] if wizard.model and hasattr(self.env[wizard.model],
+                                                                             'message_post') else self.env[
+                'mail.thread']
+            if wizard.composition_mode == 'mass_post':
+                # do not send emails directly but use the queue instead
+                # add context key to avoid subscribing the author
+                ActiveModel = ActiveModel.with_context(mail_notify_force_send=False, mail_create_nosubscribe=True)
+            # wizard works in batch mode: [res_id] or active_ids or active_domain
+            if mass_mode and wizard.use_active_domain and wizard.model:
+                res_ids = self.env[wizard.model].search(safe_eval(wizard.active_domain)).ids
+            elif mass_mode and wizard.model and self._context.get('active_ids'):
+                res_ids = self._context['active_ids']
+            else:
+                res_ids = [wizard.res_id]
+
+            batch_size = int(self.env['ir.config_parameter'].sudo().get_param('mail.batch_size')) or self._batch_size
+            sliced_res_ids = [res_ids[i:i + batch_size] for i in range(0, len(res_ids), batch_size)]
+
+            if wizard.composition_mode == 'mass_mail' or wizard.is_log or (
+                    wizard.composition_mode == 'mass_post' and not wizard.notify):  # log a note: subtype is False
+                subtype_id = False
+            elif wizard.subtype_id:
+                subtype_id = wizard.subtype_id.id
+            else:
+                subtype_id = self.env['ir.model.data'].xmlid_to_res_id('mail.mt_comment')
+
+            for res_ids in sliced_res_ids:
+                batch_mails = Mail
+                all_mail_values = wizard.get_mail_values(res_ids)
+                for res_id, mail_values in all_mail_values.items():
+                    if wizard.composition_mode == 'mass_mail':
+                        batch_mails |= Mail.create(mail_values)
+                    else:
+                        post_params = dict(
+                            message_type=wizard.message_type,
+                            subtype_id=subtype_id,
+                            notif_layout=notif_layout,
+                            add_sign=not bool(wizard.template_id),
+                            mail_auto_delete=wizard.template_id.auto_delete if wizard.template_id else False,
+                            model_description=model_description,
+                            **mail_values)
+                        if ActiveModel._name == 'mail.thread' and wizard.model:
+                            post_params['model'] = wizard.model
+                        ActiveModel.browse(res_id).message_post(**post_params)
+
+                if wizard.composition_mode == 'mass_mail':
+                    batch_mails.send(auto_commit=auto_commit)
 
 
