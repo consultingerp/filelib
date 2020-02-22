2cae6e1fd240d6b87d6f1f045d2f4d839f6d9d5e
Feng Zhou <feng.zhou1@pactera.com>
2020-02-22 19:19:54 +0800

    测试代码还原。

--- e2yun_addons/odoo12/e2yun_supplier_info/models/res_users.py
+++ e2yun_addons/odoo12/e2yun_supplier_info/models/res_users.py
@@ -9,9 +9,3 @@ class res_users(models.Model):
     vat = fields.Char('统一社会信用代码')
     company_name = fields.Char('公司名称')
 
-    @api.model
-    def create(self, values):
-        print("=================================创建用户开始==================================", values)
-        user = super(res_users, self).create(values)
-        print("=================================创建用户结束==================================", user)
-        return user
