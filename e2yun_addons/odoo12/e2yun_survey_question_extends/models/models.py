63136905dd8034effe5ab1e9e73ec43599ebea62
Kangyu.Wang <kangyu.wang@pactera.com>
2020-02-27 21:20:36 +0800

    题库大类字段给默认值，设置必输

--- e2yun_addons/odoo12/e2yun_survey_question_extends/models/models.py
+++ e2yun_addons/odoo12/e2yun_survey_question_extends/models/models.py
@@ -21,7 +21,7 @@ class e2yun_survey_question_extends(models.Model):
     # type_id = fields.Many2one('question.type', string='问题类型')
     # type_name = fields.Char(string='问题类型')
     question_bank_type = fields.Selection([('供应商基本信息', '供应商基本信息'), ('人口属性', '人口属性'), ('市场调研', '市场调研')
-                                              , ('用户满意度', '用户满意度'), ('联系方式', '联系方式'), ('其他', '其他')], string='题库大类', required=True)
+                                              , ('用户满意度', '用户满意度'), ('联系方式', '联系方式'), ('其他', '其他')], default='供应商基本信息', string='题库大类', required=True)
     type = fields.Selection([
         ('free_text', 'Multiple Lines Text Box'),
         ('textbox', 'Single Line Text Box'),
