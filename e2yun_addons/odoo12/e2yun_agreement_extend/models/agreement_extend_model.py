0bb67f7c0f4dd8861b3686da05d21545bc24c5a8
chuqiu <981274333@qq.com>
2020-03-06 19:35:01 +0800

    显示问题

--- e2yun_addons/odoo12/e2yun_agreement_extend/models/agreement_extend_model.py
+++ e2yun_addons/odoo12/e2yun_agreement_extend/models/agreement_extend_model.py
@@ -395,7 +395,7 @@ class Agreement(models.Model):
         up_sequence={}
         for agreement_data in agreement_datas:
             #阶段审批邮件提醒
-          if int(agreement_data.stage_id)<5:
+          if int(agreement_data.stage_id)<4:
             tier_review_datas = tier_review_obj.search(
                 [('res_id', '=', agreement_data.id)], order="sequence asc")
 
